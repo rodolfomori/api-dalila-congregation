12ed73b5b73d2b2334ed8e64f9b07ee7977a8c87
Rodolfo Mori <rod_dolpho@hotmail.com>
2020-03-16 20:16:18 -0300

    add cors

--- src/routes.js
+++ src/routes.js
@@ -17,7 +17,7 @@ import ActivityController from './app/controllers/ActivityController';
 const routes = new Router();
 
 // Then pass them to cors:
-express().use(cors('https://viladalila.site'));
+express().use(cors('https://viladalila.site/'));
 
 routes.post('/session', SessionController.store);
 routes.post('/users', UserController.store);
